0a36e7d9ef4b8e59029e266592859102f2ded798
Cuukel_15 <sow.pape-bilaly@ugb.edu.sn>
2023-01-01 20:44:30 +0000

    upload

--- PiscineGo#10.go
+++ PiscineGo#10.go
@@ -6,15 +6,26 @@ import (
 	"unicode/utf8"
 )
 
-// PrintRune prints a single rune (Unicode code point) and returns any error
-// if the encoding or the writing fails.
+// PrintRune prints a single rune (Unicode code point) and returns any error if the encoding or the writing fails.
 func PrintRune(r rune) error {
 	l := utf8.RuneLen(r)
 	if l == -1 {
-		return errors.New("the rune is not a valid value to encode in UTF-8")
+		return errors.New("ERROR: invalid value to encode in UTF-8")
 	}
 	p := make([]byte, l)
 	utf8.EncodeRune(p, r)
 	_, err := os.Stdout.Write(p)
+	return err
+}
+
+// PrintStr prints a string et and returns any error if the writing fails.
+func PrintStr(str string) error {
+	var err error
+	for _, r := range str {
+		err = PrintRune(r)
+		if err != nil {
+			break
+		}
+	}
 	return err
 }
---
+++ checker/main.go
@@ -0,0 +1,4 @@
+package main
+
+// Just for tests
+func main() {}
